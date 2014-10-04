d4c60958065ebc55af7863788798f3a329f6c69a
Pavel Mrlik <hello@maxifick.com>
2014-10-04 15:22:48 +0100

    changes

--- index.php
+++ index.php
@@ -1,5 +1,19 @@
 <?php
 
+$raw_xml = file_get_contents("http://feeds.bbci.co.uk/news/scotland/rss.xml");
+
+$xml = simplexml_load_string($raw_xml);
+
+$raw_json = json_encode($xml);
+
+$json = json_decode($raw_json);
+
+$items = $json->channel->item;
+
+foreach( $items as $item ) {
+	echo $item->title."<br>";
+}
+
 $data = '
 	{ 
 		"token" : "b5089dd0-b9cd-4938-bca0-354d1f778a0e", 
@@ -12,7 +26,7 @@ $data = '
 
 $ch = curl_init();
 
-curl_setopt($ch, CURLOPT_URL,            "http://widge.herokuapp.com/widgets/welcome" );
+curl_setopt($ch, CURLOPT_URL,            "http://widge.herokuapp.com/widgets/rss-news" );
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1 );
 curl_setopt($ch, CURLOPT_POST,           1 );
 curl_setopt($ch, CURLOPT_HTTPHEADER,     array('Content-Type: text/json')); 
